6f8e4f2e21fe12b69300073a363b383cd97e9ba8
ashuyadav93 <ashish.yadav.1@stonybrook.edu>
2017-11-05 04:04:35 -0500

    containment hash

--- containmenthash/src/containment.cpp
+++ containmenthash/src/containment.cpp
@@ -7,25 +7,93 @@
 #include "../../murmurhash3/murmurhash3.h"
 #include <cstring>
 #include <limits>
-#include "CountEstimator.cpp"
+#include "../../src/CountEstimator.cpp"
+#include "bloom_filter.hpp"
+#include <cmath>
+#include <set>
+using namespace std;
 
 void log(std::string s);
 
 std::vector <std::string> get_all_kmers(std::string input, unsigned int k_size);
 
 int main(int argc, char **argv) {
-    std::vector <std::string> kmers = get_all_kmers("hellosaraj", 3);
-    for (std::vector<std::string>::iterator it = kmers.begin(); it != kmers.end(); ++it) {
-        log(*it + "\n");
+    std::hash <std::string> hash_fn;
+    CountEstimator *ce1 = new CountEstimator(5, 3, true, 0, false, hash_fn);
+    ce1->add_sequence("ATTATTATTATT");
+    ce1->print_sketch();
+   
+    CountEstimator *ce2 = new CountEstimator(5, 3, true, 0, false, hash_fn);
+    ce2->add_sequence("ATTATTATTATT");
+    ce2->print_sketch();
+
+    bloom_parameters parameters;
+
+    //How many elements roughly do we expect to insert?
+    parameters.projected_element_count = 1000;
+
+    //Maximum tolerable false positive probability? (0,1)
+    parameters.false_positive_probability = 0.001; // 1 in 10000
+
+    //Simple randomizer (optional)
+    parameters.random_seed = 0xA5A5A5A5;
+
+    if (!parameters)
+    {
+        std::cout << "Error - Invalid set of bloom filter parameters!" << std::endl;
+        return 1;
     }
-    char *inp_buf = "hello";
-    void *out_buf = malloc(strlen(inp_buf) * sizeof(char));
-    MurmurHash3_x64_128(inp_buf, strlen(inp_buf), 123213, out_buf);
-    long hash_val = (long) out_buf;
-    std::cout << "Hash is " << hash_val;
-    CountEstimator *ce = new CountEstimator(10, 3, true, 0, false);
-    ce->add("llo");
-    ce->print_sketch();
+
+    parameters.compute_optimal_parameters();
+
+    //Instantiate Bloom Filter
+    bloom_filter filter(parameters);
+    std::vector<std::string>str_lista = get_all_kmers("ATTATTATTATT", 3);
+    std::vector<std::string>str_listb = ce2->get_kmers();
+    vector<string> kmer_lista = get_all_kmers("ATTATTATTATT", 3);
+    vector<string> kmer_listb = get_all_kmers("ATTATTATTATT", 3);
+    set<string>kmer_a (kmer_lista.begin(), kmer_lista.end());
+    set<string>kmer_b (kmer_listb.begin(), kmer_listb.end());
+    vector<string>intersection,union_set;
+
+    set_intersection(kmer_a.begin(),kmer_a.end(),kmer_b.begin(),kmer_b.end(), std::back_inserter(intersection));
+    set_union(kmer_a.begin(),kmer_a.end(),kmer_b.begin(),kmer_b.end(), std::back_inserter(union_set));
+    double true_jaccard = intersection.size()/double(union_set.size());
+
+    int intersect_est = 0; // intersection estimate
+    int kmer_len = 0;
+   
+    // Insert into Bloom Filter
+    
+    for (int i = 0; i < str_lista.size(); ++i)
+    {   
+        if(!filter.contains(str_lista[i])) {
+            kmer_len += 1;
+            filter.insert(str_lista[i]);
+        }
+    }
+
+    for (int i = 0; i < str_listb.size(); ++i)
+    {
+        std::cout << str_listb[i] << " ";
+        if (filter.contains(str_listb[i]))
+        {
+            std::cout << "BF contains: " << str_listb[i] << std::endl;
+            intersect_est+=1;
+        }
+    }
+
+    int h = 5;
+    // cout<< intersect_est << " " << h;
+    intersect_est -= round(parameters.false_positive_probability*h);  // adjust for the false positive rate
+    cout << intersect_est << " ";
+    double containment_est = intersect_est / double(h);
+    double jaccard_est = str_listb.size() * containment_est / (kmer_len + str_listb.size() - str_listb.size() * containment_est);  // estimate of the containment index
+    std::cout<< containment_est << std::endl;
+    std::cout<< jaccard_est << std::endl;
+    std::cout<< ce1->calc_jaccard_distance(ce2) << std::endl;
+    std::cout<< true_jaccard << endl;
+
     return 0;
 }
 
